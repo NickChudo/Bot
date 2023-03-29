8e167e613bd7b60185a2071466bd89e07b3fad0c
Vladisvell <vladisvell@gmail.com>
2023-03-29 17:15:50 +0500

    Тестовый коммит

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ bot = telebot.TeleBot('6213357069:AAFrHUa2DaU0XdiMOAvibA_Tb3iBuwQin2Y')
 def get_text_messages(message):
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "Привет, ага":
+        bot.send_message(message.from_user.id, "Ахахахахахахахахахахахаха!")
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Напиши привет")
     else:
