fb9bc9c3ccbbcda0077ce40e094a70a88b247bd9
Vladisvell <vladisvell@gmail.com>
2023-04-21 00:51:57 +0500

    now with downloading voice messages

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import telebot
+import soundfile as sf
 
 bot = telebot.TeleBot('6213357069:AAFrHUa2DaU0XdiMOAvibA_Tb3iBuwQin2Y')
+
+samplerate = 16000 #set this constant to that number which is compatible with network, yep.
 
 @bot.message_handler(content_types=['text'])
 
@@ -17,5 +20,14 @@ def get_text_messages(message):
 @bot.message_handler(content_types=['voice'])
 def solve_voice_message(message):
     bot.send_message(message.from_user.id, "Recieved voice message. Nice.")
+    file_metadata = bot.get_file(message.voice.file_id)
+    file = bot.download_file(file_metadata.file_path)
+    #TODO: find a better way to convert ogg's to wav's
+    with open('example.ogg', 'wb') as new_file:
+        new_file.write(file)
+    #TODO: unhardcode this    
+    data, samplerate = sf.read('example.ogg')
+    sf.write('example.wav',data, samplerate)
+
     
 bot.polling(none_stop=True, interval=0)
